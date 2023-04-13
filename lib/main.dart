ee24f62c85222519a62eceeebff7943dea3f2a1b
DevEduu <contatoradarcriativo@gmail.com>
2023-04-13 17:49:55 -0300

    Add Class Transaction

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,10 +16,31 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+  final _transaction = Transactions(
+    id: 't1',
+    title: 'Novo Tênis de corrida',
+    value: 300.10,
+    date: DateTime.now(),
+  );
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Despesas Pessoais'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Card(
+              child: Text('Gráfico'),
+              elevation: 5,
+            ),
+            Card(
+              child: Text('Lista de Transações'),
+            ),
+          ],
+        ));
   }
 }
