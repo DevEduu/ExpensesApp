99010f28be86edfa2ebc8d7740a73899330d4dc1
DevEduu <contatoradarcriativo@gmail.com>
2023-05-04 21:20:34 -0300

    Refatorando

--- lib/components/Transaction_list.dart
+++ lib/components/Transaction_list.dart
@@ -1,6 +1,5 @@
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 
 class TransactionList extends StatelessWidget {
--- lib/main.dart
+++ lib/main.dart
@@ -78,8 +78,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    bool _islandscape =
-        MediaQuery.of(context).orientation == Orientation.landscape;
+    final mediaQuery = MediaQuery.of(context);
+    bool _islandscape = mediaQuery.orientation == Orientation.landscape;
     AppBar appBar = AppBar(
       centerTitle: true,
       actions: [
@@ -100,14 +100,14 @@ class _MyHomePageState extends State<MyHomePage> {
       title: Text(
         'Despesas Pessoais',
         style: TextStyle(
-          fontSize: 20 * MediaQuery.of(context).textScaleFactor,
+          fontSize: 20 * mediaQuery.textScaleFactor,
         ),
       ),
     );
 
-    final availableHeight = MediaQuery.of(context).size.height -
+    final availableHeight = mediaQuery.size.height -
         appBar.preferredSize.height -
-        MediaQuery.of(context).padding.top;
+        mediaQuery.padding.top;
 
     return Scaffold(
       appBar: appBar,
